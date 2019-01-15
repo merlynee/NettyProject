3e06b6651b195a3b214b5f3150d9c57ebc610dcd
刘平 <liu.ping@ebiz-interactive.com>
2019-01-15 17:04:42 +0800

    123

---
+++ NettyHelloWorld/src/main/java/com/merlynee/server/NettyServer.java
@@ -0,0 +1,40 @@
+package com.merlynee.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Liu.Ping
+ * @time 2019-1-15 下午 4:39
+ * @description
+ */
+public class NettyServer {
+
+    public void start() throws Exception {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(group)
+                    .channel(NioSctpServerChannel.class)
+                    .localAddress(new InetSocketAddress(1))
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(serverHandler);
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind().sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+}
